306cb4356405348f9e5eae9b828d0ac66922e8b5
hossam-basudan <the.hosam@yahoo.com>
2018-08-01 18:07:15 +0200

    started linker

--- contextStudio.vue
+++ contextStudio.vue
@@ -1,70 +1,24 @@
 
 <template>
-  <svg class="contextStudio"
-       width="960"
-       height="600"></svg>
+  <md-content>
+    <context-list :contextList="contextList"
+                  :editMode="false"></context-list>
+  </md-content>
 
 </template>
 
 <script >
-import * as d3 from "d3";
 import { SpinalGraph } from "spinal-models-context_studio";
+import contextList from "./manager/contextList.vue";
 var globalType = typeof window === "undefined" ? global : window;
 var spinalSystem;
-
-var graph = {
-  nodes: [
-    {
-      id: "Myriel",
-      group: 1,
-      depth: 1
-    },
-    {
-      id: "Labarre",
-      group: 50,
-      depth: 3
-    },
-    {
-      id: "Napoleon",
-      group: 1,
-      depth: 1
-    },
-    {
-      id: "Valjean",
-      group: 30,
-      depth: 4
-    },
-    {
-      id: "Marguerite",
-      group: 3,
-      depth: 1
-    }
-  ],
-  links: [
-    {
-      source: "Napoleon",
-      target: "Myriel",
-      value: 1
-    },
-    {
-      source: "Valjean",
-      target: "Labarre",
-      value: 1
-    },
-    {
-      source: "Marguerite",
-      target: "Valjean",
-      value: 1
-    }
-  ]
-};
 
 export default {
   name: "contextStudio",
   data() {
-    return {};
+    return { contextList: null };
   },
-  components: {},
+  components: { contextList },
   methods: {
     getEvents: function() {},
     linkToDB: function() {
@@ -74,14 +28,16 @@ export default {
           if (forgefile) {
             if (forgefile.graph) {
               forgefile.graph.init();
+              if (typeof forgefile.graph.appsList !== "undefined")
+                this.contextList = forgefile.graph.appsList;
             } else {
-              console.log(forgefile.name.get());
-
               let g = new SpinalGraph(forgefile.name.get());
               g.init();
               forgefile.add_attr({
                 graph: g
               });
+              if (typeof g.appsList !== "undefined")
+                this.contextList = g.appsList;
             }
           }
         })
@@ -94,134 +50,64 @@ export default {
     spinalSystem = globalType.spinal.spinalSystem;
     this.getEvents();
     this.linkToDB();
-
-    var radius = 12;
-
-    var svg = d3.select(".contextStudio"),
-      width = +svg.attr("width"),
-      height = +svg.attr("height");
-
-    var color = d3.scaleOrdinal(d3.schemeCategory10);
-    var depth = d3.scaleLinear().range([5, radius]);
-
-    var simulation = d3
-      .forceSimulation()
-      .force(
-        "link",
-        d3.forceLink().id(function(d) {
-          return d.id;
-        })
-      )
-      .force("charge", d3.forceManyBody())
-      .force("center", d3.forceCenter(width / 2, height / 2));
-
-    var link = svg
-      .append("g")
-      .attr("class", "links")
-      .selectAll("line")
-      .data(graph.links)
-      .enter()
-      .append("line")
-      .attr("stroke-width", function(d) {
-        return Math.sqrt(d.value);
-      });
-
-    var node = svg
-      .append("g")
-      .attr("class", "nodes")
-      .selectAll("g")
-      .data(graph.nodes)
-      .enter()
-      .append("g");
-
-    var circles = node
-      .append("circle")
-      .attr("r", function(d) {
-        return depth(d.depth);
-      })
-      .attr("fill", function(d) {
-        let test = color(d.group);
-        return test;
-      })
-      .call(
-        d3
-          .drag()
-          .on("start", dragstarted)
-          .on("drag", dragged)
-          .on("end", dragended)
-      );
-
-    var lables = node
-      .append("text")
-      .text(function(d) {
-        return d.id;
-      })
-      .attr("x", 6)
-      .attr("y", 3);
-
-    node.append("title").text(function(d) {
-      return d.id;
-    });
-
-    simulation.nodes(graph.nodes).on("tick", ticked);
-
-    simulation.force("link").links(graph.links);
-
-    function ticked() {
-      link
-        .attr("x1", function(d) {
-          return d.source.x;
-        })
-        .attr("y1", function(d) {
-          return d.source.y;
-        })
-        .attr("x2", function(d) {
-          return d.target.x;
-        })
-        .attr("y2", function(d) {
-          return d.target.y;
-        });
-
-      node.attr("transform", function(d) {
-        return "translate(" + d.x + "," + d.y + ")";
-      });
-      // .attr("cx", function(d) { return d.x; })
-      // .attr("cy", function(d) { return d.y; });
-    }
-
-    function dragstarted(d) {
-      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-      d.fx = d.x;
-      d.fy = d.y;
-    }
-
-    function dragged(d) {
-      d.fx = d3.event.x;
-      d.fy = d3.event.y;
-    }
-
-    function dragended(d) {
-      if (!d3.event.active) simulation.alphaTarget(0);
-      d.fx = null;
-      d.fy = null;
-    }
   }
 };
 </script>
 
-<style>
-.links line {
-  stroke: #999;
-  stroke-opacity: 0.6;
+<style scoped>
+.contextStudio {
+  height: calc(100% - 35px);
+}
+</style>
+
+<style  >
+.contextStudio button.md-icon-button.md-button.md-theme-default {
+  min-width: 20px;
+  width: 20px;
+  height: 20px;
 }
 
-.nodes circle {
-  stroke: #fff;
-  stroke-width: 1.5px;
+.contextStudio .md-toolbar {
+  height: 20px;
+  min-height: 20px;
 }
 
-text {
-  font-family: sans-serif;
-  font-size: 10px;
+.contextStudio .md-title {
+  font-size: 15px;
+}
+
+.contextStudio .md-list,
+.md-list *,
+.md-button,
+.md-button * {
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  padding-right: 0px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  padding-left: 0px;
+}
+
+.contextStudio i.md-icon.md-icon-font.md-theme-default {
+  font-size: 18px !important;
+}
+
+.contextStudio span {
+  font-size: 14px;
+}
+
+.contextStudio .md-ripple.md-list-item-content {
+  min-height: 0px;
+  font-size: 14px;
+  box-sizing: border-box;
+}
+
+.small-menu * {
+  min-height: 0px;
+  font-size: 14px;
+  box-sizing: border-box;
+}
+.editModeStyle {
+  background-color: #356bab;
 }
 </style>
---
+++ d3.vue
@@ -0,0 +1,226 @@
+
+<template>
+  <svg class="contextStudio"
+       width="960"
+       height="600"></svg>
+
+</template>
+
+<script >
+import * as d3 from "d3";
+import { SpinalGraph } from "spinal-models-context_studio";
+var globalType = typeof window === "undefined" ? global : window;
+var spinalSystem;
+
+var graph = {
+  nodes: [
+    {
+      id: "Myriel",
+      group: 1,
+      depth: 1
+    },
+    {
+      id: "Labarre",
+      group: 50,
+      depth: 3
+    },
+    {
+      id: "Napoleon",
+      group: 1,
+      depth: 1
+    },
+    {
+      id: "Valjean",
+      group: 30,
+      depth: 4
+    },
+    {
+      id: "Marguerite",
+      group: 3,
+      depth: 1
+    }
+  ],
+  links: [
+    {
+      source: "Napoleon",
+      target: "Myriel",
+      value: 1
+    },
+    {
+      source: "Valjean",
+      target: "Labarre",
+      value: 1
+    },
+    {
+      source: "Marguerite",
+      target: "Valjean",
+      value: 1
+    }
+  ]
+};
+
+export default {
+  name: "contextStudio",
+  data() {
+    return {};
+  },
+  components: {},
+  methods: {
+    getEvents: function() {},
+    linkToDB: function() {
+      spinalSystem
+        .getModel()
+        .then(forgefile => {
+          if (forgefile) {
+            if (forgefile.graph) {
+              forgefile.graph.init();
+            } else {
+              console.log(forgefile.name.get());
+              let g = new SpinalGraph(forgefile.name.get());
+              g.init();
+              forgefile.add_attr({
+                graph: g
+              });
+            }
+          }
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }
+  },
+  mounted() {
+    spinalSystem = globalType.spinal.spinalSystem;
+    this.getEvents();
+    this.linkToDB();
+
+    var radius = 12;
+
+    var svg = d3.select(".contextStudio"),
+      width = +svg.attr("width"),
+      height = +svg.attr("height");
+
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
+    var depth = d3.scaleLinear().range([5, radius]);
+
+    var simulation = d3
+      .forceSimulation()
+      .force(
+        "link",
+        d3.forceLink().id(function(d) {
+          return d.id;
+        })
+      )
+      .force("charge", d3.forceManyBody())
+      .force("center", d3.forceCenter(width / 2, height / 2));
+
+    var link = svg
+      .append("g")
+      .attr("class", "links")
+      .selectAll("line")
+      .data(graph.links)
+      .enter()
+      .append("line")
+      .attr("stroke-width", function(d) {
+        return Math.sqrt(d.value);
+      });
+
+    var node = svg
+      .append("g")
+      .attr("class", "nodes")
+      .selectAll("g")
+      .data(graph.nodes)
+      .enter()
+      .append("g");
+
+    var circles = node
+      .append("circle")
+      .attr("r", function(d) {
+        return depth(d.depth);
+      })
+      .attr("fill", function(d) {
+        let test = color(d.group);
+        return test;
+      })
+      .call(
+        d3
+          .drag()
+          .on("start", dragstarted)
+          .on("drag", dragged)
+          .on("end", dragended)
+      );
+
+    var lables = node
+      .append("text")
+      .text(function(d) {
+        return d.id;
+      })
+      .attr("x", 6)
+      .attr("y", 3);
+
+    node.append("title").text(function(d) {
+      return d.id;
+    });
+
+    simulation.nodes(graph.nodes).on("tick", ticked);
+
+    simulation.force("link").links(graph.links);
+
+    function ticked() {
+      link
+        .attr("x1", function(d) {
+          return d.source.x;
+        })
+        .attr("y1", function(d) {
+          return d.source.y;
+        })
+        .attr("x2", function(d) {
+          return d.target.x;
+        })
+        .attr("y2", function(d) {
+          return d.target.y;
+        });
+
+      node.attr("transform", function(d) {
+        return "translate(" + d.x + "," + d.y + ")";
+      });
+      // .attr("cx", function(d) { return d.x; })
+      // .attr("cy", function(d) { return d.y; });
+    }
+
+    function dragstarted(d) {
+      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+      d.fx = d.x;
+      d.fy = d.y;
+    }
+
+    function dragged(d) {
+      d.fx = d3.event.x;
+      d.fy = d3.event.y;
+    }
+
+    function dragended(d) {
+      if (!d3.event.active) simulation.alphaTarget(0);
+      d.fx = null;
+      d.fy = null;
+    }
+  }
+};
+</script>
+
+<style>
+.links line {
+  stroke: #999;
+  stroke-opacity: 0.6;
+}
+
+.nodes circle {
+  stroke: #fff;
+  stroke-width: 1.5px;
+}
+
+text {
+  font-family: sans-serif;
+  font-size: 10px;
+}
+</style>
