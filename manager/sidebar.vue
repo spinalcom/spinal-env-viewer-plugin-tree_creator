636d5f722951b6a22e0a0c0cb0e76956786ffe3a
Loris Ciprin <loris@ciprin.fr>
2018-09-21 18:14:59 +0200

    create function getChildren from relation

--- manager/sidebar.vue
+++ manager/sidebar.vue
@@ -366,8 +366,7 @@
    console.log("relations object", node.relations[relationName]);
    console.log("relations children", node.relations[relationName][0].nodeList2);
    getChildFromRelation(node.relations[relationName][0].nodeList2[0], "hasRoom>");
-   getChildFromRelation(node.relations[relationName][0].nodeList2[0], "hasEquipment>")
-
+   getChildFromRelation(node.relations[relationName][0].nodeList2[0], "hasEquipment>");
   }
 
   /**
